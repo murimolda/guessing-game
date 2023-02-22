1c8da22bf0790f42d3b402d4cf660608f1ee7f93
Marina Palter <murimolda@gmail.com>
2023-02-22 15:25:41 +0400

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessValue = Math.round(((this.max - this.min) / 2) + this.min);
+        return this.guessValue;
     }
 
     lower() {
-
+        this.max = this.guessValue;
     }
 
     greater() {
-
+        this.min = this.guessValue;
     }
 }
 
